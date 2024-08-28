7b246c877c67cc36224f6c6461aef194ac19b06a
yeseol <1999onell@gmail.com>
2024-08-28 15:07:54 +0900

    first commit

---
+++ src/main/java/com/dororok/drumsheetmusic/DrumsheetmusicApplication.java
@@ -0,0 +1,12 @@
+package com.dororok.drumsheetmusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrumsheetmusicApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DrumsheetmusicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dororok/drumsheetmusic/DrumsheetmusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dororok.drumsheetmusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrumsheetmusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
