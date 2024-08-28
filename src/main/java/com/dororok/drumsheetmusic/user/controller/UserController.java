366996b2a5048a55214e64639216519fbd4fc5ce
yeseol <1999onell@gmail.com>
2024-08-28 17:31:34 +0900

    프로젝트 구조 잡기

---
+++ src/main/java/com/dororok/drumsheetmusic/user/controller/UserController.java
@@ -0,0 +1,9 @@
+package com.dororok.drumsheetmusic.user.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+//    @GetMapping('/')
+}
